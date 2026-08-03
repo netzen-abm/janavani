f1c7143c6f759da6ce3289cea84f3ccc8b5f5092
NetZen <229501415+netzen-abm@users.noreply.github.com>
2026-08-04 00:48:41 +0530

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,15 @@
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    PORT = int(os.getenv("PORT", 10000))
