b844dd5befb01272065dbf44efdf49b639e9dd24
NetZen <229501415+netzen-abm@users.noreply.github.com>
2026-08-04 00:49:28 +0530

    Update supabase.py

--- src/database/supabase.py
+++ src/database/supabase.py
@@ -1,1 +1,13 @@
+from supabase import create_client
 
+from core.config import Config
+
+
+supabase = None
+
+if Config.SUPABASE_URL and Config.SUPABASE_ANON_KEY:
+
+    supabase = create_client(
+        Config.SUPABASE_URL,
+        Config.SUPABASE_ANON_KEY
+    )
