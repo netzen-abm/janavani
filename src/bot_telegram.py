bfad67d1efa9a9f5087e569b7bb20b60cc4a3bca
NetZen <229501415+netzen-abm@users.noreply.github.com>
2026-08-04 20:06:19 +0530

    Update bot_telegram.py

--- src/bot_telegram.py
+++ src/bot_telegram.py
@@ -15,7 +15,7 @@ from commands.start import start
 from commands.search import search
 from commands.rate import rate
 
-# from commands.complaint import complaint
+from commands.complaint import complaint
 
 # ----------------------------
 # Conversation Router
