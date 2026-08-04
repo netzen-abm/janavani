03ec249bb84f05d4165a34d55cfeb9397e974f3b
NetZen <229501415+netzen-abm@users.noreply.github.com>
2026-08-04 15:30:57 +0530

    Update search.py

--- src/commands/search.py
+++ src/commands/search.py
@@ -1,1 +1,54 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from tools.search_directory import search_office
+
+
+async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    # ------------------------------------
+    # Validate Input
+    # ------------------------------------
+
+    if len(context.args) < 2:
+
+        await update.message.reply_text(
+            """
+Usage
+
+/search department location
+
+Example
+
+/search ration Kochi
+
+/search village Kannur
+
+/search police Kozhikode
+"""
+        )
+
+        return
+
+    # ------------------------------------
+    # Read Arguments
+    # ------------------------------------
+
+    department = context.args[0]
+
+    location = " ".join(context.args[1:])
+
+    # ------------------------------------
+    # Search Office
+    # ------------------------------------
+
+    result = search_office(
+        department,
+        location,
+    )
+
+    # ------------------------------------
+    # Reply
+    # ------------------------------------
+
+    await update.message.reply_text(result)
