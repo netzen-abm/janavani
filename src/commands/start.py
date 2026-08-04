faebb3091a97d2f6b7b00532c4f19d236d43e282
NetZen <229501415+netzen-abm@users.noreply.github.com>
2026-08-04 15:09:29 +0530

    Update start.py

--- src/commands/start.py
+++ src/commands/start.py
@@ -1,1 +1,32 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    message = """
+🇮🇳 Welcome to Janavani
+
+Citizen Governance Platform
+
+Available Commands
+
+/start
+/search
+/rate
+/complaint
+
+Or simply type your problem.
+
+Examples
+
+Broken Road
+
+Water Pipe Leakage
+
+Ration Card Delay
+
+Aadhaar not updated
+"""
+
+    await update.message.reply_text(message)
