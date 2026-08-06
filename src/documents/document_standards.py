84cfdf60d0ae9aa2072dc571bda7b2ef85ba4d36
NetZen <229501415+netzen-abm@users.noreply.github.com>
2026-08-06 14:59:46 +0530

    Create document_standards.py

---
+++ src/documents/document_standards.py
@@ -0,0 +1,15 @@
+class DocumentStandard:
+
+    include_disclaimer = True
+
+    include_submission_metadata = True
+
+    include_acknowledgement = True
+
+    include_response_deadline = True
+
+    include_reference_number = True
+
+    include_enclosures = True
+
+    include_copy_to = True
