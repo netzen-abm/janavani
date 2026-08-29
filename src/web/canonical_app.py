81a0c4c7a83717acfb0c5f137c33b0905d44c75f
NetZen <mycoach808@proton.me>
2026-08-29 11:47:31 +0530

    feat(webapp): wire civic action route into canonical API

--- src/web/canonical_app.py
+++ src/web/canonical_app.py
@@ -8,6 +8,7 @@ from src.web.legislative_router import router as legislative_router
 from src.web.constitutional_router import router as constitutional_router
 from src.web.land_router import router as land_router
 from src.web.case_router import router as case_router
+from src.web.civic_action_router import router as civic_action_router
 
 
 def create_canonical_app() -> FastAPI:
@@ -17,6 +18,7 @@ def create_canonical_app() -> FastAPI:
     app.include_router(constitutional_router)
     app.include_router(land_router)
     app.include_router(case_router)
+    app.include_router(civic_action_router)
 
     @app.get("/liveness", tags=["Platform"])
     async def liveness() -> dict[str, str]:
